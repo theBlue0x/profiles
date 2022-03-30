245bb691134e7305c34920b0dfb54a2060542167
theBlue0x <theBlue0x@users.noreply.github.com>
2022-03-30 16:25:41 -0400

    Initial

---
+++ constants.js
@@ -0,0 +1,26 @@
+var Constants = (function(Constants, $, undefined) {
+
+//Constants.username = 'ILikeTurtles';
+  Constants.username = 'ILikeTurtles';
+
+//Constants.city = 'New York';
+  Constants.city = 'New York';
+
+//Constants.address= 'BLX-FYHQ-E8G4-TS8H-9C26A';
+  Constants.address= 'BLX-FYHQ-E8G4-TS8H-9C26A';
+
+//Constants.gravatarURL = 'username@email.com';
+  Constants.gravatarURL = 'contact@blue0x.com';
+
+//Constants.twitterURL = 'https://twitter.com/username';
+  Constants.twitterURL = 'https://twitter.com/blue0x_com';
+
+//Constants.githubURL = 'https://github.com/username';
+  Constants.githubURL = 'https://github.com/theBlue0x';
+
+//Constants.facebookURL = 'https://facebook.com/username';
+  Constants.facebookURL = 'https://facebook.com';
+
+  return Constants;
+
+} (Constants || {}, jQuery));
---
+++ js/flip.js
@@ -0,0 +1,21 @@
+var card = document.querySelector('.card-container')
+var button = document.querySelector('button.primary.ghost');
+var button2 = document.getElementById('backbutton');
+
+button.addEventListener( 'click', function() {
+  var x = document.getElementById("back");
+  var y = document.getElementById("front");
+    card.classList.toggle('is-flipped');
+    setTimeout(() => { y.style.display = "none"; }, 500);
+    setTimeout(() => { x.style.display = "block"; }, 500);
+    
+});
+
+button2.addEventListener( 'click', function() {
+  var x = document.getElementById("back");
+  var y = document.getElementById("front");
+    card.classList.toggle('is-flipped');
+    setTimeout(() => { x.style.display = "none"; }, 500);
+    setTimeout(() => { y.style.display = "block"; }, 555);
+    
+});
